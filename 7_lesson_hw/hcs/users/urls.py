7b6419c45d0176124aa24dd996ad4de9fcd52109
Ilya Panin <prokyror_311200@mail.ru>
2021-11-20 19:55:31 +0300

    Fix the name of the user-info url

--- 7_lesson_hw/hcs/users/urls.py
+++ 7_lesson_hw/hcs/users/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('create/', UsersCreateView.as_view(), name='create_user'),
     path('all/', UsersListView.as_view(), name='all_users'),
     path('delete/<int:pk>/', UsersDeleteView.as_view(), name='delete_user'),
-    path('update/username-info/<int:pk>/', UsersUpdateUserInfoView.as_view(), name='update_userinfo'),
+    path('update/user-info/<int:pk>/', UsersUpdateUserInfoView.as_view(), name='update_userinfo'),
     path('update/username/<int:pk>/', UserUpdateUsernameView.as_view(), name='update_username'),
     path('update/password/<int:pk>/', UserUpdatePasswordView.as_view(), name='update_password'),
 
