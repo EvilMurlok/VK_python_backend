d43eeaaef7339242c7978d18a0ba89c69961fc23
Ilya Panin <prokyror_311200@mail.ru>
2021-12-11 13:30:46 +0300

    Some small fixes

--- 4_lesson_hw/hcs/news/tasks.py
+++ 4_lesson_hw/hcs/news/tasks.py
@@ -1,5 +1,5 @@
-from celery.schedules import crontab
 from django.core.mail import send_mail
+
 from application.settings import DEFAULT_FROM_EMAIL, ADMIN_EMAILS, BANNED_WORDS_PATH
 from application.celery import app
 
@@ -24,13 +24,3 @@ def moderate_news_titles():
             if banned_word in news.content.lower():
                 news.content = news.content.replace(banned_word, '*' * len(banned_word))
         news.save()
-
-
-# app.conf.beat_schedule.update(
-#     {
-#         'moderate-news-titles': {
-#             'task': 'news.tasks.moderate_news_titles',
-#             'schedule': crontab(minute='*', hour='*', day_of_month='*', month_of_year='*', day_of_week='*')
-#         }
-#     }
-# )
